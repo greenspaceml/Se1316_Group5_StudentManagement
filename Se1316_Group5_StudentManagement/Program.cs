e88c90ff759cb70e8fa81f5e98e7492e20ee1320
greenspaceml <greenspaceml@gmail.com>
2020-04-03 21:51:22 +0700

    Add project files.

---
+++ Se1316_Group5_StudentManagement/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Se1316_Group5_StudentManagement {
+    static class Program {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
