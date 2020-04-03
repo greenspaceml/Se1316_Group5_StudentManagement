357ea573aa7824b81a066e88d5ca4ba1dbfa6944
greenspaceml <greenspaceml@gmail.com>
2020-04-04 02:15:29 +0700

    added Delete, Edit, Insert, Filter Student functions

--- Se1316_Group5_StudentManagement/DAL/StudentDAO.cs
+++ Se1316_Group5_StudentManagement/DAL/StudentDAO.cs
@@ -15,5 +15,34 @@ namespace Se1316_Group5_StudentManagement.DAL {
             string cmd = "Select * from Student";
             return DAO.GetDataTable(cmd);
         }
+        public static bool InsertStudent_Quang(string Name, int Gender, string DoB,string Addess, string Phone, string Email) {
+            SqlCommand cmd = new SqlCommand(" insert into Student ([Name],Gender,DoB,Addess,Phone,Email) " +
+                                            " values (@Name,@Gender,@DoB,@Addess,@Phone,@Email)");
+            cmd.Parameters.AddWithValue("@Name", Name);
+            cmd.Parameters.AddWithValue("@Gender", Gender);
+            cmd.Parameters.AddWithValue("@DoB", DoB);
+            cmd.Parameters.AddWithValue("@Addess", Addess);
+            cmd.Parameters.AddWithValue("@Phone", Phone);
+            cmd.Parameters.AddWithValue("@Email", Email);
+            return DAO.UpdateTable(cmd);
+        }
+        public static bool DeleteStudent(string studentID) {
+            SqlCommand cmd = new SqlCommand("delete from Student where StudentID=@studentID");
+            cmd.Parameters.AddWithValue("@studentID", studentID);
+            return DAO.UpdateTable(cmd);
+        }
+
+        public static bool UpdateStudent_Quang(string studentID, string Name, int Gender, string DoB, string Addess, string Phone, string Email) {
+            SqlCommand cmd = new SqlCommand(" update Student set [Name] =@Name ,Gender=@Gender ,DoB=@DoB ,Addess=@Addess ,Phone=@Phone ,Email=@Email  " +
+                                            " where StudentID=@studentID");
+            cmd.Parameters.AddWithValue("@Name", Name);
+            cmd.Parameters.AddWithValue("@Gender", Gender);
+            cmd.Parameters.AddWithValue("@DoB", DoB);
+            cmd.Parameters.AddWithValue("@Addess", Addess);
+            cmd.Parameters.AddWithValue("@Phone", Phone);
+            cmd.Parameters.AddWithValue("@Email", Email);
+            cmd.Parameters.AddWithValue("@studentID", studentID);
+            return DAO.UpdateTable(cmd);
+        }
     }
 }
--- Se1316_Group5_StudentManagement/GUI/StudentGUI.cs
+++ Se1316_Group5_StudentManagement/GUI/StudentGUI.cs
@@ -22,23 +22,48 @@ namespace Se1316_Group5_StudentManagement.GUI {
         }
         void setEnableOFF() {
             txtStudentName.Enabled = false;
-            txtStudentGender.Enabled = false;
             txtStudentDOB.Enabled = false;
             txtStudentAddress.Enabled = false;
             txtStudentPhone.Enabled = false;
             txtStudentEmail.Enabled = false;
             btnEdit.Enabled = false;
             btnDelete.Enabled = false;
-            btnSave.Enabled = false;
+            rdbFemale.Enabled = false;
+            rdbMale.Enabled = false;
+        }
+        void setEnableForAddnew(bool va) {
+            txtStudentName.Enabled = va;
+            txtStudentDOB.Enabled = va;
+            txtStudentAddress.Enabled = va;
+            txtStudentPhone.Enabled = va;
+            txtStudentEmail.Enabled = va;
+            rdbFemale.Enabled = va;
+            rdbMale.Enabled = va;
+            btnDelete.Enabled = !va;
+            btnEdit.Enabled = !va;
+        }
+        void setEnableForEdit(bool va) {
+            txtStudentName.Enabled = va;
+            txtStudentDOB.Enabled = va;
+            txtStudentAddress.Enabled = va;
+            txtStudentPhone.Enabled = va;
+            txtStudentEmail.Enabled = va;
+            rdbFemale.Enabled = va;
+            rdbMale.Enabled = va;
+            btnDelete.Enabled = !va;
+            btnAdd.Enabled = !va;
         }
         private void dataGridViewStudent_CellClick(object sender, DataGridViewCellEventArgs e) {
             txtStudentID.Text = dataGridViewStudent.Rows[e.RowIndex].Cells["StudentID"].Value.ToString();
             txtStudentName.Text = dataGridViewStudent.Rows[e.RowIndex].Cells["Name"].Value.ToString();
-            txtStudentGender.Text = dataGridViewStudent.Rows[e.RowIndex].Cells["Gender"].Value.ToString();
+            rdbMale.Checked = (bool)dataGridViewStudent.Rows[e.RowIndex].Cells["Gender"].Value;
+            rdbFemale.Checked = !(bool)dataGridViewStudent.Rows[e.RowIndex].Cells["Gender"].Value;
             txtStudentDOB.Text = dataGridViewStudent.Rows[e.RowIndex].Cells["DoB"].Value.ToString();
             txtStudentAddress.Text = dataGridViewStudent.Rows[e.RowIndex].Cells["Addess"].Value.ToString();
             txtStudentPhone.Text = dataGridViewStudent.Rows[e.RowIndex].Cells["Phone"].Value.ToString();
             txtStudentEmail.Text = dataGridViewStudent.Rows[e.RowIndex].Cells["Email"].Value.ToString();
+            btnDelete.Enabled = true;
+            btnEdit.Enabled = true;
         }
 
         private void btnFindStudent_Click(object sender, EventArgs e) {
@@ -48,5 +73,39 @@ namespace Se1316_Group5_StudentManagement.GUI {
             dataGridViewStudent.DataSource = dv;
         }
 
+        private void btnAdd_Click(object sender, EventArgs e) {
+            if (btnAdd.Text == "Add New Student") {
+                btnAdd.Text = "Save";
+                setEnableForAddnew(true);
+            } else {
+                int gen = rdbMale.Checked ? 1 : 0;
+                bool checking = StudentDAO.InsertStudent_Quang(txtStudentName.Text, gen, txtStudentDOB.Text, txtStudentAddress.Text, txtStudentPhone.Text, txtStudentEmail.Text);
+                btnAdd.Text = "Add New Student";
+                setEnableForAddnew(false);
+                MessageBox.Show(checking ? "New student added successful!" : "Student adding failed!");
+                LoadStudentGridView();
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e) {
+            bool checking = StudentDAO.DeleteStudent(txtStudentID.Text);
+            MessageBox.Show(checking ? "Student delete successful!" : "Student delete failed!");
+            LoadStudentGridView();
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e) {
+            if (btnEdit.Text == "Edit Student") {
+                btnEdit.Text = "Update";
+                setEnableForEdit(true);
+            } else {
+                setEnableForEdit(false);
+                int gen = rdbMale.Checked ? 1 : 0;
+                btnEdit.Text = "Edit Student";
+                bool checking = StudentDAO.UpdateStudent_Quang(txtStudentID.Text, txtStudentName.Text, gen, txtStudentDOB.Text, txtStudentAddress.Text, txtStudentPhone.Text, txtStudentEmail.Text);
+                MessageBox.Show(checking ? "Student Updated successful!" : "Student Updated failed!");
+                LoadStudentGridView();
+            }
+
+        }
     }
 }
